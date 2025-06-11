5620306edf5f66e74f3d7d2d7483e7a570a79361
Ashish Kumar Mishra <ashishmishra19122000@gmail.com>
2025-06-11 23:57:20 +0530

    Delete healthcare-dashboard directory

--- healthcare-dashboard/src/data/calendarAppointments.js
+++
@@ -1,9 +0,0 @@
-export const calendarData = [
-  { date: '25', slots: [] },
-  { date: '26', slots: ['08:00', '09:00', '10:00'] },
-  { date: '27', slots: ['11:00', '12:00'] },
-  { date: '28', slots: ['13:00', '14:00'] },
-  { date: '29', slots: ['15:00'] },
-  { date: '30', slots: ['09:00', '11:00'] },
-  { date: '31', slots: [] },
-];
